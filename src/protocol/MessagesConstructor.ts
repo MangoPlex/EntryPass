d8fb1fa4dbb616fac00f87a18452eb65a54a487c
nahkd123 <nahkd123@gmail.com>
2021-09-22 22:19:00 +0700

    Update on QR Code messaging protocol

--- src/protocol/MessagesConstructor.ts
+++ src/protocol/MessagesConstructor.ts
@@ -3,14 +3,18 @@ import { Pass } from "../passes/Pass";
 import { ByteInputStream } from "../streams/ByteInputStream";
 import { UserInformation } from "../passes/UserInformation";
 import { TransmitType } from "./TransmitType";
+import { Certificate } from "..";
 
 /**
  * Messages constructor to construct messages for QR codes
  */
 export namespace MessagesConstructor {
 
+    // "PASS" + messageHeader + trailing data...
+    export const QRCODE_HEADER = new Uint8Array([0x50, 0x41, 0x53, 0x53]);
+
     export function messageHeader(compressed: boolean, id: number) {
-        return (compressed? 0b1000_0000 : 0) | id;
+        return (compressed? 0b1000_0000 : 0) | (id & 0xF);
     }
 
     export function parseHeader(byte: number): [number, boolean] {
@@ -19,29 +23,75 @@ export namespace MessagesConstructor {
         return [id, compressed];
     }
 
-    export function sendUserInfo(info: UserInformation) {
-        let stream = new ByteOutputStream(256);
-        stream.writeByte(messageHeader(false, TransmitType.USER_INFO));
+    export function requestUserInfo() {
+        return new Uint8Array([
+            ...QRCODE_HEADER,
+            messageHeader(false, TransmitType.REQUEST_USER_INFO)
+        ]);
+    }
+
+    export function streamOf(id: number) {
+        let stream = new ByteOutputStream(128);
+        stream.writeBytes(QRCODE_HEADER);
+        stream.writeByte(messageHeader(false, id));
+        return stream;
+    }
+
+    export function requestPassCreation(info: UserInformation) {
+        let stream = streamOf(TransmitType.REQUEST_PASS_CREATION);
         info.writeToStream(stream);
         return stream.toBytes();
     }
 
-    export function sendPass(pass: Pass) {
-        let stream = new ByteOutputStream(256);
-        stream.writeByte(messageHeader(false, TransmitType.PASS_INFO));
+    export function importPass(pass: Pass) {
+        let stream = streamOf(TransmitType.PASS_IMPORT);
         pass.writeToStream(stream);
         return stream.toBytes();
     }
 
-    export function processMessage(msg: Uint8Array) {
+    export function showPass(pass: Pass) {
+        let stream = streamOf(TransmitType.PASS_SHOW);
+        pass.writeToStream(stream);
+        return stream.toBytes();
+    }
+
+    export function requestRootTrust(root: Certificate) {
+        if (root.parent) throw new Error("The given certificate must be a root");
+        if (!root.hasSignature) throw new Error("The given certificate must be self-signed");
+        let stream = streamOf(TransmitType.REQUEST_ROOT_TRUST);
+        root.writeToStream(stream);
+        return stream.toBytes();
+    }
+
+    export function processQRCodeMessage(msg: Uint8Array) {
+        for (let i = 0; i < QRCODE_HEADER.length; i++) if (msg[i] != QRCODE_HEADER[i]) return null;
         let stream = new ByteInputStream(msg);
-        let compressed: boolean, id: number;
-        [id, compressed] = parseHeader(stream.readByte());
-        switch (id) {
-        case TransmitType.USER_INFO: return UserInformation.readFromStream(stream);
-        case TransmitType.PASS_INFO: return Pass.readFromStream(stream);
-        default: throw new Error("Unknown ID: " + id);
+        stream.pointer += QRCODE_HEADER.length;
+
+        let msgObj = <Message> { data: msg };
+        [msgObj.id, msgObj.compressed] = parseHeader(stream.readByte());
+        switch (msgObj.id) {
+        case TransmitType.REQUEST_USER_INFO: msgObj.info = UserInformation.readFromStream(stream); break;
+
+        case TransmitType.PASS_IMPORT:
+        case TransmitType.PASS_SHOW: msgObj.pass = Pass.readFromStream(stream); break;
+
+        case TransmitType.REQUEST_ROOT_TRUST: msgObj.certificate = Certificate.readFromStream(stream); break;
+
+        case TransmitType.REQUEST_PASS_CREATION: break;
+        default: return null;
         }
+        return msgObj;
+    }
+
+    export interface Message {
+        compressed: boolean;
+        id: number;
+        data: Uint8Array;
+
+        certificate?: Certificate;
+        info?: UserInformation;
+        pass?: Pass;
     }
 
 }
--- src/protocol/TransmitType.ts
+++ src/protocol/TransmitType.ts
@@ -3,10 +3,22 @@
  */
 export namespace TransmitType {
 
-    /** Transmit user's information (mostly for generating passes) */
-    export const USER_INFO = 0x0;
+    /** Request user's info. No other trailing data required */
+    export const REQUEST_USER_INFO = 0x0;
 
-    /** Transmit user's pass information (for checking) */
-    export const PASS_INFO = 0x1;
+    /** Request pass creation. The trailing data is the user's info */
+    export const REQUEST_PASS_CREATION = 0x1;
+
+    /** Import pass from trailing data */
+    export const PASS_IMPORT = 0x2;
+
+    /** Show pass though trailing data for verification */
+    export const PASS_SHOW = 0x3;
+
+    /** Request certificate root trust (Only needed for verifiers) */
+    export const REQUEST_ROOT_TRUST = 0x4;
+
+    /** Custom communication though Entry Pass protocol */
+    export const CUSTOM = 0xF;
 
 }
